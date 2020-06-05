ece17fc73c1658b5cdb29598f1725748f5779d8d
Prasham <prasham.jain@myrealdata.net>
2020-06-05 19:21:12 +0530

    day 14 work

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { HelperService } from '../helper.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-chat',
@@ -6,6 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+
+  public groupList: Array<any>;
+  public chatList: Array<any>;
+
+  public currentRoom;   // room where user is currently in 
+
+  public typingUser: String = '';
+  public message: String = '';
+
+  public lastTimestamp: Number = 0;
+  private userId: String;
+  private username: String;
+
+  // modal variables
+  public forNew: Boolean = false;
+  public roomId;  // used for edit and delete
+  public roomName;
+  public active: String;
+  public saveButtonText: String;
+
+  public prevName: String;
+  public prevActive: String;
 
   constructor() { }
 
